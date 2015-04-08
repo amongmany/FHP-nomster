d3de491f6906a78c68b7f8bd803c5ee6d236bdd8
amongmany <danny@amongmany.com>
2015-04-08 14:25:02 +0000

    User show controller test working. humanized rating test still fucked.

---
+++ test/factories.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+	factory :comment do
+		rating "5_stars"
+		association :user
+		association :place
+	end
+
+	factory :user do
+		sequence :email do |n|
+			"person#{n}@example.com"
+		end
+		password "password"
+		password_confirmation "password"
+	end
+
+	factory :place do
+		name "The Blue Stove"
+		description "Awesome baked stuff"
+		latitude(42.3631519)
+		longitude(-71.056098)
+		association :user
+	end
+
+end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "humanized rating" do
+  	comment = FactoryGirl.create(:comment, :rating => '5_stars', :user => user, :place => place)
+  	expected = "five stars"
+  	actual = comment.humanized_rating
+  	assert_equal expected, actual
+
+  end
 end
